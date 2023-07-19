ed46c47d01cb131ffaaeea69d5e1b146eec350b9
KyleM021199 <kylem021199@gmail.com>
2023-07-18 20:36:25 -0500

    fixing indexDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,10 +16,11 @@ const initdb = async () =>
 export const putDb = async (content) =>{
   console.log('PUT to the database');
   const jateDB = await openDB ('jate', 1);
-  const tx = jateDB.transaction('jate', 'readonly');
+  const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({id: id, jate: content});
+  const request = store.put({id: 1, value: content});
   const result = await request;
+  console.log('The data was saved to database', result.value);
 }; //console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -28,11 +29,15 @@ console.log('Get all from the database');
 const jateDB = await openDB ('jate', 1);
 const tx = jateDB.transaction('jate', 'readonly');
 const store = tx.objectStore('jate');
-const request = store.getAll();
+const request = store.get(1);
 const result = await request;
-console.log("result.value", result);
-return result;
-
+console.log("result.value", result.value);
+if (result){
+console.log('Data gotten from the database', result.value)
+} else{
+  console.log('Data was not found');
+}
+return result?.value;
 }; //console.error('getDb not implemented');
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
@@ -23,10 +24,25 @@ module.exports = () => {
         title: 'JATE'
       }),
       new WebpackPwaManifest({
-
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Text editor for your needs.',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
       new InjectManifest({
-
+        swSrc:'./src-sw.js',
+        swDest: 'src-sw.js',
       })
     ],
 
@@ -37,12 +53,17 @@ module.exports = () => {
           use: ['style-loader', 'css-loader'],
         },
         {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             }
           }
         },
