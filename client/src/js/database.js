7ab2d1a077d48be405208e1154e80b28372c3e4c
KyleM021199 <kylem021199@gmail.com>
2023-07-20 09:10:03 -0500

    updated README

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//  A method that accepts some content and adds it to the database
 export const putDb = async (content) =>{
   console.log('PUT to the database');
   const jateDB = await openDB ('jate', 1);
@@ -21,9 +21,9 @@ export const putDb = async (content) =>{
   const request = store.put({id: 1, value: content});
   const result = await request;
   console.log('The data was saved to database', result.value);
-}; //console.error('putDb not implemented');
+};
 
-// TODO: Add logic for a method that gets all the content from the database
+// A method that gets all the content from the database
 export const getDb = async () =>{
 console.log('Get all from the database');
 const jateDB = await openDB ('jate', 1);
@@ -38,6 +38,6 @@ console.log('Data gotten from the database', result.value)
   console.log('Data was not found');
 }
 return result.value;
-}; //console.error('getDb not implemented');
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,19 +1,19 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// An event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
 event.preventDefault();
 butInstall.style.visibility = 'visible';
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+//  A click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
 butInstall.setAttribute('disabled',true);
 butInstall.textContent = 'Installed';
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// An handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
 console.log('Success!', 'appinstalled', event);
 butInstall.setAttribute('disabled',false);
